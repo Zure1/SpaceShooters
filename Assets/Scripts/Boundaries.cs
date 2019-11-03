7f55c0e9987b979ed2cad177d5840103746253ee
Dennis Schröer <Luchsia@Freenet.de>
2019-11-03 18:06:25 +0100

    Added Player Ship, added boundaries
    Playership sprite & GameObject added, added Bouncaries for Playership

---
+++ Assets/Scripts/Boundaries.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class Boundaries : MonoBehaviour
+{
+    public Camera MainCamera;
+    Vector2 screenBounds;
+    float objectWidth, objectHeight;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        screenBounds = MainCamera.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, MainCamera.transform.position.z));
+        objectWidth = transform.GetComponent<SpriteRenderer>().bounds.extents.x; //extents = size of width / 2
+        objectHeight = transform.GetComponent<SpriteRenderer>().bounds.extents.y; //extents = size of height / 2
+    }
+
+    // Update is called once per frame
+    private void LateUpdate()
+    {
+        Vector3 viewPos = transform.position;
+        viewPos.x = Mathf.Clamp(viewPos.x, screenBounds.x * -1 + objectWidth, screenBounds.x - objectWidth);
+        viewPos.y = Mathf.Clamp(viewPos.y, screenBounds.y * -1 + objectHeight, screenBounds.y - objectHeight);
+        transform.position = viewPos;
+    }
+}
---
+++ Assets/Scripts/PlayerShip.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+
+public class PlayerShip : MonoBehaviour
+{
+    float speed = 0.06f; // how far the paddle moves per frame
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.A))
+        {
+            MoveLeft();
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            MoveRight();
+        }
+
+        if (Input.GetKey(KeyCode.Space))
+        {
+
+        }
+    }
+
+    void MoveLeft()
+    {
+        transform.position = new Vector2(transform.position.x - speed, transform.position.y);
+    }
+
+    void MoveRight()
+    {
+        transform.position = new Vector2(transform.position.x + speed, transform.position.y);
+    }
+
+    /// <summary>
+    /// Shoots from the ship's position.
+    /// </summary>
+    void Shoot()
+    {
+
+    }
+}
