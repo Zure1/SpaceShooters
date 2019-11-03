13bed50e9d68d6597659ae10fd66d38b91947f47
Dennis Schröer <Luchsia@Freenet.de>
2019-11-03 18:51:38 +0100

    Added Fire Button, Destroy Laser when out of Boundaries

---
+++ Assets/LaserScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LaserScript : MonoBehaviour
+{
+    Vector2 screenBounds;
+    Vector3 laserDirection = Vector3.up;
+    Vector3 movePosition;
+    float laserHeight;
+    float laserSpeed = 0.10f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        screenBounds = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, Camera.main.transform.position.z));
+        laserHeight = transform.GetComponent<SpriteRenderer>().bounds.extents.y;
+        movePosition = new Vector3(transform.position.x, screenBounds.y + laserHeight);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 newPos = Vector3.MoveTowards(transform.position, movePosition, laserSpeed);
+        transform.position = newPos;
+
+        if (transform.position == movePosition)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
--- Assets/Scripts/PlayerShip.cs
+++ Assets/Scripts/PlayerShip.cs
@@ -3,16 +3,28 @@
 public class PlayerShip : MonoBehaviour
 {
     float speed = 0.06f; // how far the paddle moves per frame
+    float shipHeight;    
+    public GameObject Laser1;
+
+    Vector3 movePosition = new Vector3(10f, 10f, 10f);
+
+    GameObject shot;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        shipHeight = transform.GetComponent<SpriteRenderer>().bounds.extents.y;
     }
 
     // Update is called once per frame
     void Update()
     {
+        //if (shot != null)
+        //{
+        //    Vector3 newPos = Vector3.MoveTowards(shot.transform.position, movePosition, laserSpeed);
+        //    shot.transform.position = newPos;
+        //}
+
         if (Input.GetKey(KeyCode.A))
         {
             MoveLeft();
@@ -22,9 +34,9 @@ public class PlayerShip : MonoBehaviour
             MoveRight();
         }
 
-        if (Input.GetKey(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space))
         {
-
+            Shoot();
         }
     }
 
@@ -43,6 +55,6 @@ public class PlayerShip : MonoBehaviour
     /// </summary>
     void Shoot()
     {
-
+        shot = Instantiate(Laser1, new Vector3(transform.position.x, transform.position.y + shipHeight + Laser1.transform.GetComponent<SpriteRenderer>().bounds.extents.y), Quaternion.identity);
     }
 }
